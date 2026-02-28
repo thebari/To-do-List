34688b5aad6a168a484eb2afe3885f1ed2013a14
bari <179538475+thebari@users.noreply.github.com>
2026-02-28 00:26:02 -0300

    Versão 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const input = document.getElementById("input");
 const buttonAdd = document.getElementById("btnAdicionar");
+const item = document.getElementById("item");
 
 function addItem(){
     const text = input.value.trim();
-
-    if (text == "") return;
+    if (text === "") return;
 
     const li = document.createElement("li");
     li.textContent = text;
@@ -13,13 +13,12 @@ function addItem(){
     btnRemover.textContent = "X";
     
     btnRemover.addEventListener("click", function(){
-        
-
+        li.remove();
     })
 
-    
     li.appendChild(btnRemover);
-    document.getElementById("item").appendChild(li);
+    item.appendChild(li);
+
     input.value = "";
 }
 
