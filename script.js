90239769227c22af73ba82c2541cb9c648c74d85
bari <179538475+thebari@users.noreply.github.com>
2026-03-03 11:02:51 -0300

    Versão 1.4 - Adicionei cor e organizei as features

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 const input = document.getElementById("input");
 const buttonAdd = document.getElementById("btnAdicionar");
-const item = document.getElementById("item");
+const list = document.getElementById("list");
 
 function addItem() {
-    if (input.value.trim() === "") {
-        return;
-    }
+  if (input.value.trim() === "") {
+    return;
+  }else{
 
-    const li = document.createElement("li");
-    li.textContent = input.value.trim();
+  const li = document.createElement("li");
+  li.textContent = input.value.trim();
 
-    const btnCompletar = document.createElement("button");
-    btnCompletar.textContent = "Completar";
-    li.appendChild(btnCompletar);
+  const btnCompletar = document.createElement("button");
+  btnCompletar.innerHTML = '<i class="fas fa-check"></i>';
+  li.appendChild(btnCompletar);
 
-    const btnRemover = document.createElement("button");
-    btnRemover.textContent = "Remover";
-    li.appendChild(btnRemover);
+  const btnRemover = document.createElement("button");
+  btnRemover.innerHTML = '<i class="fas fa-trash"></i>';
+  li.appendChild(btnRemover);
 
-    btnCompletar.addEventListener("click", function () {
-        li.classList.toggle("completo");
-    });
+  btnCompletar.addEventListener("click", function () {
+    li.classList.toggle("completo");
+  });
 
-    btnRemover.addEventListener("click", function () {
-        li.remove();
-    });
+  btnRemover.addEventListener("click", function () {
+    li.remove();
+  });
 
-    item.appendChild(li);
-    input.value = "";
+  list.appendChild(li);
+  input.value = "";
+  }
 }
 
 buttonAdd.addEventListener("click", addItem);
